576ca482eacdcc07a0553b9335f5aede78c6358f
KMH8359 <gnslgns2000@naver.com>
2019-06-10 10:23:41 +0900

    .
    .

--- finalProject/finalProject/mainGame.cpp
+++ finalProject/finalProject/mainGame.cpp
@@ -119,6 +119,9 @@ void mainGame::update()				//연산 함수
 	if (KEYMANAGER->isOnceKeyDown(VK_SPACE)) {
 		E_Manager->KillGreenTurtle();
 	}
+	if (KEYMANAGER->isOnceKeyDown(VK_HOME)) {
+		O_Manger->changeToGreenTurtle();
+	}
 
 	coin->update();
 	Firan->update();
